771cd70266d7df90fb6d0ca1d74312bed0860ccc
alanr <alanrios2001@gmail.com>
2024-04-24 23:54:31 -0300

    declarações

--- app_projeto/models.py
+++ app_projeto/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Listas(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Itens(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    id_lista = models.ForeignKey(Listas, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
